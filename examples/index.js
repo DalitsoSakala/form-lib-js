961db134f8816c0cfa5d4942fec8bba723034795
Dalitso Sakala <dalitso.1sc@gmail.com>
2023-07-16 00:47:05 +0200

    Updated example

--- examples/index.js
+++ examples/index.js
@@ -1,27 +1,45 @@
 let { Form } = require('../dist/index')
+let fs = require('fs')
+let { join } = require('path')
 
 let schema = {
-    name: String,
-    gender: String,
-    isRegistered: Boolean,
-    age: {
-        type: Number,
-        min: 0,
+    libraryName: {
+        default: '@jsnodor',
+        type: String,
         required: true
     },
-    date_of_birth: Date
+    applicableEnvironment: {
+        type: String,
+        default: 'web',
+        choices: ['node', 'web'],
+    },
+    configurationRequired: Boolean,
+    numberOfVersions: {
+        type: Number,
+        min: 0,
+        required: true,
+        enum: /\w/
+    },
+    date_created: {
+        type: Date,
+        default: new Date
+    }
 }
 
 
 
 class MyForm extends Form {
+    constructor() {
+        super(null)
+        this.formCssClass = 'form-horizontal'
+        this.fieldCssClass = 'd-block'
+    }
     configure(tag) {
         return {
             tag,
             schema,
-            fields:['name','age','date_of_birth']
         }
     }
 }
 
-console.log(new MyForm().asDiv() + '')
+fs.writeFileSync(join(__dirname, 'example.html'), '<!-- Example form -->' + new MyForm().asDiv(), { encoding: 'utf-8' })
